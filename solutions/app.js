470e3950abf22ee6c0b83f828047211f2b0842b6
daniel <danielwine@tutamail.com>
2021-11-17 20:24:00 +0100

    All done.

--- solutions/app.js
+++ solutions/app.js
@@ -1,1 +1,8 @@
 
+import setCookie from './app1.js';
+import { cookieHandler } from './app2.js';
+
+export {
+    setCookie,
+    cookieHandler
+}
--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,10 @@
 
+const setCookie = (str) => {
+    let cookieString = `token=${str}; `
+    const now = new Date()
+    now.setTime(now.getTime() + 15 * 60 * 1000)
+    cookieString += `expires=${now.toUTCString()}; path=/`
+    document.cookie = cookieString;
+}
+
+export default setCookie;
